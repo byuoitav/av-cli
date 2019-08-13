9a97899a041461f4eefda70f843c3b8e9560aba3
CalebLarsen <caleb.john.larsen@gmail.com>
2019-08-13 16:43:23 -0600

    For Danny

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -77,14 +77,7 @@ var downCmd = &cobra.Command{
 	Short: "runs docker-compose down",
 	Args:  args.Valid,
 	Run: func(cmd *cobra.Command, args []string) {
-		//tempDockerHubTag := os.Getenv("DOCKER_HUB_TAG")
-		//tempSystemID := os.Getenv("SYSTEM_ID")
-
-		//os.Setenv("DOCKER_HUB_TAG", designation)
-		//os.Setenv("SYSTEM_ID", args[0])
 		out, err := exec.Command("docker-compose", "-f", fmt.Sprintf("%v/src/github.com/byuoitav/av-api/docker-compose-pull.yml", os.Getenv("GOPATH")), "down").CombinedOutput()
-		//os.Setenv("DOCKER_HUB_TAG", tempDockerHubTag)
-		//os.Setenv("SYSTEM_ID", tempSystemID)
 		if err != nil {
 			fmt.Printf("Error running command: %v %s\n", err, out)
 			os.Exit(1)
