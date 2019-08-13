95f02ff0f98c913ebf8a0d1a725dd070bb8f441d
CalebLarsen <caleb.john.larsen@gmail.com>
2019-08-13 17:08:22 -0600

    using the right db

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/byuoitav/av-cli/cmd/args"
 	arg "github.com/byuoitav/av-cli/cmd/args"
@@ -31,20 +32,20 @@ var upCmd = &cobra.Command{
 			fmt.Printf("error getting designation: %v", err)
 			os.Exit(1)
 		}
-
+		var dockerDesignation string
 		switch designation {
 		case "dev":
-			designation = "development"
+			dockerDesignation = "development"
 		case "stg":
-			designation = "stage"
+			dockerDesignation = "stage"
 		case "prd":
-			designation = "latest"
+			dockerDesignation = "latest"
 		}
 
 		tempDockerHubTag := os.Getenv("DOCKER_HUB_TAG")
 		tempSystemID := os.Getenv("SYSTEM_ID")
 
-		os.Setenv("DOCKER_HUB_TAG", designation)
+		os.Setenv("DOCKER_HUB_TAG", dockerDesignation)
 		os.Setenv("SYSTEM_ID", args[0])
 		c := exec.Command("docker-compose", "-f", fmt.Sprintf("%v/src/github.com/byuoitav/av-api/docker-compose-pull.yml", os.Getenv("GOPATH")), "pull")
 		c.Stdout = os.Stdout
@@ -59,8 +60,16 @@ var upCmd = &cobra.Command{
 
 		tempDockerHubTag = os.Getenv("DOCKER_HUB_TAG")
 		tempSystemID = os.Getenv("SYSTEM_ID")
+		tempDBAddress := os.Getenv("DB_ADDRESS")
+		tempDBName := os.Getenv("DB_USERNAME")
 
-		os.Setenv("DOCKER_HUB_TAG", designation)
+		os.Setenv("DB_USERNAME", designation)
+		finalAddr := strings.Replace(os.Getenv("DB_ADDRESS"), "dev", designation, 1)
+		finalAddr = strings.Replace(finalAddr, "stg", designation, 1)
+		finalAddr = strings.Replace(finalAddr, "prd", designation, 1)
+		os.Setenv("DB_ADDRESS", finalAddr)
+
+		os.Setenv("DOCKER_HUB_TAG", dockerDesignation)
 		os.Setenv("SYSTEM_ID", args[0])
 		c = exec.Command("docker-compose", "-f", fmt.Sprintf("%v/src/github.com/byuoitav/av-api/docker-compose-pull.yml", os.Getenv("GOPATH")), "up", "-d")
 		c.Stdout = os.Stdout
@@ -68,6 +77,8 @@ var upCmd = &cobra.Command{
 		err = c.Run()
 		os.Setenv("DOCKER_HUB_TAG", tempDockerHubTag)
 		os.Setenv("SYSTEM_ID", tempSystemID)
+		os.Setenv("DB_USERNAME", tempDBName)
+		os.Setenv("DB_ADDRESS", tempDBAddress)
 		if err != nil {
 			fmt.Printf("Error running command: %v\n", err)
 			os.Exit(1)
