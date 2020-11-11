f95c1b8fa89b0b94d9659d05ff177a74941e5125
Daniel Randall <dgrandall@me.com>
2020-11-11 13:01:44 -0700

    terraform

--- terraform/slack.tf
+++ terraform/slack.tf
@@ -16,7 +16,7 @@ module "slack_cli" {
   // required
   name           = "slack-cli"
   image          = "docker.pkg.github.com/byuoitav/av-cli/slack-dev"
-  image_version  = "v0.1.8-beta"
+  image_version  = "1df616a"
   container_port = 8080
   repo_url       = "https://github.com/byuoitav/av-cli"
 
