0b9b2ab990432ca2b51a47b5ca54a9a52f2e3906
Clinton Reeder <clinton_reeder@byu.edu>
2021-11-17 16:25:05 -0700

    Added logic for Ansible handling

--- server/float.go
+++ server/float.go
@@ -5,11 +5,22 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	avcli "github.com/byuoitav/av-cli"
 	"go.uber.org/zap"
 )
+
+func checkSubstrings(str string, subs ...string) bool {
+	isCompleteMatch := true
+
+	for _, sub := range subs {
+		if strings.Contains(str, sub) {
+
+		}
+	}
+}
 
 func (s *Server) Float(id *avcli.ID, stream avcli.AvCli_FloatServer) error {
 	log := s.log(stream.Context())
@@ -20,16 +31,28 @@ func (s *Server) Float(id *avcli.ID, stream avcli.AvCli_FloatServer) error {
 	defer cancel()
 
 	return s.runPerPi(ctx, id, stream, func(pi avcli.Pi) error {
+		// Buildings that have been converted over to use Ansible
+		var buildings = []string{"ITB", "JKB"}
+
 		log := log.With(zap.String("pi", pi.ID))
 
 		// no individual pi can take longer than 30 seconds
 		ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 		defer cancel()
 
-		req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("https://api.byu.edu/domains/av/flight-deck/%v/webhook_device/%v", id.Designation, pi.ID), nil)
-		if err != nil {
-			log.Warn("unable to build request", zap.Error(err))
-			return fmt.Errorf("unable to build request: %w", err)
+		for _, building := range buildings {
+			if strings.Contains(pi.ID, building) {
+				req, err := http.NewRequestWithContext(ctx, "POST", fmt.Sprintf("https://api.byu.edu/domains/av/flight-deck/dev/refloat/%v", pi.ID))
+				if err != nil {
+					log.Warn("Unable to build request: %w", err)
+				}
+			} else {
+				req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("https://api.byu.edu/domains/av/flight-deck/%v/webhook_device/%v", id.Designation, pi.ID), nil)
+				if err != nil {
+					log.Warn("unable to build request", zap.Error(err))
+					return fmt.Errorf("unable to build request: %w", err)
+				}
+			}
 		}
 
 		resp, err := s.Client.Do(req)
