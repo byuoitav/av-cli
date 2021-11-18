147cb85a48d455aacd38fc06b95c1472aac8fd04
Clinton Reeder <clinton_reeder@byu.edu>
2021-11-18 11:45:59 -0700

    Ansible (#23)
    * Added logic for Ansible handling
    
    * Fixing logic to make selection work
    
    * Fixing scoping issue in float.go
    
    * Typing issue

--- server/float.go
+++ server/float.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	avcli "github.com/byuoitav/av-cli"
@@ -20,16 +21,39 @@ func (s *Server) Float(id *avcli.ID, stream avcli.AvCli_FloatServer) error {
 	defer cancel()
 
 	return s.runPerPi(ctx, id, stream, func(pi avcli.Pi) error {
+		// Buildings that have been converted over to use Ansible
+		var buildings = []string{"ITB", "JKB"}
+		var buildingMatch = false
+		var req *http.Request
+		var err error
+
 		log := log.With(zap.String("pi", pi.ID))
 
 		// no individual pi can take longer than 30 seconds
 		ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
 		defer cancel()
 
-		req, err := http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("https://api.byu.edu/domains/av/flight-deck/%v/webhook_device/%v", id.Designation, pi.ID), nil)
-		if err != nil {
-			log.Warn("unable to build request", zap.Error(err))
-			return fmt.Errorf("unable to build request: %w", err)
+		// Loop will interate over list of buildings and if it finds a match, it will change variable to true and break the loop
+		for _, building := range buildings {
+			if strings.Contains(pi.ID, building) {
+				buildingMatch = true
+				break
+			}
+		}
+
+		// if the building matches with the list of buildings above, run that ansible endpoint verses running the old flightdeck endpoint
+		if buildingMatch {
+			req, err = http.NewRequestWithContext(ctx, "POST", fmt.Sprintf("https://api.byu.edu/domains/av/flight-deck/dev/refloat/%v", pi.ID), nil)
+			if err != nil {
+				log.Warn("Unable to build request:", zap.Error(err))
+				return fmt.Errorf("Unable to build request: %w", err)
+			}
+		} else {
+			req, err = http.NewRequestWithContext(ctx, "GET", fmt.Sprintf("https://api.byu.edu/domains/av/flight-deck/%v/webhook_device/%v", id.Designation, pi.ID), nil)
+			if err != nil {
+				log.Warn("unable to build request", zap.Error(err))
+				return fmt.Errorf("unable to build request: %w", err)
+			}
 		}
 
 		resp, err := s.Client.Do(req)
